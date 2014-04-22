a6d833742f3ef5539c95d2ce0bc32c4066f15f57
Josh Leeman <joshleeman@gmail.com>
2014-04-21 21:10:39 -0400

    updates

--- main.rb
+++ main.rb
@@ -5,30 +5,78 @@ require 'pry'
 
 set :sessions, true
 
+# define helper methods, can also use modules for this
+
+helpers do
+
+  def init_deck
+    suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts']
+    values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+    suits.product(values).shuffle!
+  end
+
+  def first_deal(player_cards, dealer_cards, deck)
+    2.times do
+      player_cards << deck.pop
+      dealer_cards << deck.pop 
+    end
+  end
+
+  def total(h)
+    h_value = 0
+    h.each do |card|
+      value = card[1]
+      case value
+        when /[2-9]|[1][0]/ 
+          h_value += value.to_i 
+        when "Jack" || "Queen" || "King"
+          h_value += 10
+        when "A"
+          h_value += 11
+          if h_value > 21
+            h_value -= 10 
+          end 
+      end
+    end
+    h_value
+  end
+
+  def image_paths(hand)
+    paths = []
+    hand.each do |card|
+      suit = card[0].downcase.to_s
+      value = card[1].downcase.to_s
+      paths << "/images/cards/#{suit}_#{value}.jpg"
+    end
+    paths
+  end
+
+end
+
 # routing processes here, get/posts
 
 get '/' do
   if session[:user_name]
   redirect '/game'
   else
-    redirect '/set_name'
+    redirect 'player/set_name'
   end
 end
 
-get '/set_name' do
+get '/player/set_name' do
   erb :set_name
 end
 
-post '/set_name' do
+post '/player/set_name' do
   session[:user_name] = params[:user_name]
-  redirect '/set_bet'
+  redirect 'player/set_bet'
 end
 
-get '/set_bet' do
+get '/player/set_bet' do
   erb :set_bet
 end
 
-post '/set_bet' do
+post '/player/set_bet' do
   session[:bet] = params[:bet].to_i
   redirect '/game'
 end
@@ -36,30 +84,34 @@ end
 get '/game' do
   session[:money] = 500
   session[:deck] = init_deck
-  deck = session[:deck]
-  test_card = deck[8]
-  session[:img_path] = set_img_path(test_card)
-  puts session[:img_path]
+  session[:player_cards] = []
+  session[:dealer_cards] = []
+  first_deal(session[:player_cards], session[:dealer_cards], session[:deck])
+  session[:player_total] = total(session[:player_cards])
+  session[:dealer_total] = total(session[:dealer_cards])
+  session[:player_image_paths] = image_paths(session[:player_cards])
+  session[:dealer_image_paths] = image_paths(session[:dealer_cards])
+
   erb :game
 end
 
-def init_deck
-  deck = []
-  suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts']
-  values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
-  suits.each do |suit|
-    values.each do |value|
-      deck << [suit, value]
-    end
+post '/game' do
+  if params[:Hit]
+    redirect '/player/hit'
+  elsif params[:Stay]
+    redirect '/player/stay'
   end
-  deck.shuffle!
 end
 
-def set_img_path(card)
-  suit = card[0].downcase
-  value = card[1].downcase
-  img_path = "/public/images/cards/#{suit}_#{value}.jpg"
+get '/player/hit' do
+  erb :hit
 end
+
+get '/player/stay' do
+  erb :stay
+end
+
+
 
 
 
