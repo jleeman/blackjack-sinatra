fadb22c43011827c5a81f8c037550345492f600f
Josh Leeman <joshleeman@gmail.com>
2014-04-27 18:59:29 -0400

    win/lose/push helper methods, various cleanup and new routing

--- main.rb
+++ main.rb
@@ -5,6 +5,11 @@ require 'pry'
 
 set :sessions, true
 
+# constants
+
+BLACKJACK_VALUE = 21
+DEALER_HIT_VALUE = 17
+
 # define helper methods, can also use modules for this
 
 helpers do
@@ -35,20 +40,10 @@ helpers do
     end
 
     values.select{|value| value == "Ace"}.count.times do
-      break if h_total <= 21
+      break if h_total <= BLACKJACK_VALUE
       h_total -= 10
     end
     h_total
-  end
-
-  def image_paths(hand)
-    paths = []
-    hand.each do |card|
-      suit = card[0].downcase.to_s
-      value = card[1].downcase.to_s
-      paths << "/images/cards/#{suit}_#{value}.jpg"
-    end
-    paths
   end
 
   def display_card(card)
@@ -57,22 +52,37 @@ helpers do
     "<img src='/images/cards/#{suit}_#{value}.jpg' style='margin-right:10px' />"
   end
 
-  def compare(player_cards, dealer_cards)
-    if total(player_cards) > total(dealer_cards)
-      winner = "player"
-    elsif total(player_cards) == total(dealer_cards)
-      winner = "push"
-    else
-      winner = "dealer"
-    end
+  def win!(message)
+    @show_hit_stay = false
+    @show_dealer_option = false
+    @play_again = true
+    # update player money
+    session[:money] += session[:bet]
+    @success = "<strong>#{session[:player_name]} wins!!!</strong> #{message}<br  />#{session[:player_name]} now has $#{session[:money]}."
+  end
+
+  def lose!(message)
+    @show_hit_stay = false
+    @show_dealer_option = false
+    @play_again = true
+    # update player money
+    session[:money] -= session[:bet]
+    @error = "<strong>#{session[:player_name]} loses, sorry!</strong> #{message}<br  />#{session[:player_name]} now has $#{session[:money]}."
+  end
+
+  def tie!(message)
+    @show_hit_stay = false
+    @show_dealer_option = false
+    @play_again = true
+    @success = "<strong>It's a push!</strong> #{message}<br  />#{session[:player_name]} still has $#{session[:money]}."
   end
 
 end
 
 before do
-  @show_buttons = true
-  @show_dealer_option = true
-  @hide_dealer_card = true
+  @show_hit_stay = true
+  @show_dealer_option = false
+  @show_dealer_card = false
 end
 
 # routing processes here, get/posts
@@ -111,77 +121,73 @@ get '/game' do
   first_deal(session[:player_cards], session[:dealer_cards], session[:deck])
   session[:player_total] = total(session[:player_cards])
   session[:dealer_total] = total(session[:dealer_cards])
-  if total(session[:player_cards]) == 21
-    @success = "#{session[:player_name]} hit blackjack! Game over"
+  if total(session[:player_cards]) == BLACKJACK_VALUE
+    win!("#{session[:player_name]} hit blackjack!")
   end
   erb :game
 end
 
 post '/player/hit' do
   session[:player_cards] << session[:deck].pop
-  if total(session[:player_cards]) == 21
-    session[:money] += session[:bet]
-    @success = "#{session[:player_name]} hit blackjack! #{session[:player_name]} now has $#{session[:money]}."
-    @show_buttons = false
-    @show_dealer_option = false
-  elsif total(session[:player_cards]) > 21
-    session[:money] -= session[:bet]
-    @error = "Sorry, #{session[:player_name]}, you went bust! #{session[:player_name]} now has $#{session[:money]}."
-    @show_buttons = false
-    @show_dealer_option = false
+  player_total = total(session[:player_cards])
+
+  if player_total == BLACKJACK_VALUE
+    win!("#{session[:player_name]} hit blackjack!")
+  elsif player_total > BLACKJACK_VALUE
+    lose!("#{session[:player_name]} went bust at #{player_total}!")  
   end
   erb :'/game'
 end
 
 post '/player/stay' do
-  @show_buttons = false
-  @hide_dealer_card = false
-  if total(session[:dealer_cards]) > 16 && total(session[:dealer_cards]) < 21
-    winner = compare(session[:player_cards], session[:dealer_cards])
-    if winner == "player"
-      session[:money] += session[:bet]
-      @success = "Your cards more than dealer cards, you win, #{session[:player_name]}! You now have $#{session[:money]}."
-      @show_dealer_option = false
-    elsif winner == "push"
-      @error = "It's a push, #{session[:player_name]}! You still have $#{session[:money]}."
-      @show_dealer_option = false
-    else 
-      session[:money] -= session[:bet]
-      @error = "Your cards less than dealer cards, you lose, #{session[:player_name]}! You now have $#{session[:money]}."
-      @show_dealer_option = false
-    end
+  @success = "#{session[:player_name]} has chosen to stay."
+  @show_hit_stay = false
+  redirect '/dealer'
+end
+
+get '/dealer' do
+  @show_hit_stay = false
+  @show_dealer_card = true
+  dealer_total = total(session[:dealer_cards])
+  
+  if dealer_total == BLACKJACK_VALUE
+    lose!("Dealer hit blackjack.")
+  elsif dealer_total > BLACKJACK_VALUE
+    win!("Dealer busted at #{dealer_total}!")
+  elsif dealer_total >= DEALER_HIT_VALUE
+    redirect '/compare'
+  else
+    @show_dealer_option = true
   end
+
   erb :'/game'
 end
 
 post '/dealer/hit' do
   session[:dealer_cards] << session[:deck].pop
-  @show_buttons = false
-  @hide_dealer_card = false
-  if total(session[:dealer_cards]) > 21
-    session[:money] += session[:bet]
-    @success = "Dealer went bust, you win, #{session[:player_name]}! You now have $#{session[:money]}."
-    @show_dealer_option = false
-  elsif total(session[:dealer_cards]) == 21
-    session[:money] -= session[:bet]
-    @error = "Dealer hit blackjack, you lost, #{session[:player_name]}! You now have $#{session[:money]}."
-    @show_dealer_option = false
-  elsif total(session[:dealer_cards]) > 16 && total(session[:dealer_cards]) < 21
-    winner = compare(session[:player_cards], session[:dealer_cards])
-    if winner == "player"
-      session[:money] += session[:bet]
-      @success = "Your cards more than dealer cards, you win, #{session[:player_name]}! You now have $#{session[:money]}."
-      @show_dealer_option = false
-    elsif winner == "push"
-      @error = "It's a push, #{session[:player_name]}! You still have $#{session[:money]}."
-      @show_dealer_option = false
-    else       
-      session[:money] -= session[:bet]
-      @error = "Your cards less than dealer cards, you lose, #{session[:player_name]}! You now have $#{session[:money]}."
-      @show_dealer_option = false
-    end
+  redirect '/dealer'
+end
+
+get '/compare' do
+  @show_hit_stay = false
+  @show_dealer_card = true
+  @show_dealer_option = false
+  player_total = total(session[:player_cards])
+  dealer_total = total(session[:dealer_cards])
+
+  if player_total < dealer_total
+    lose!("#{session[:player_name]} stayed at #{player_total} and dealer stayed at #{dealer_total}.")
+  elsif player_total > dealer_total
+    win!("#{session[:player_name]} stayed at #{player_total} and dealer stayed at #{dealer_total}.")
+  else 
+    tie!("#{session[:player_name]} stayed at #{player_total} and dealer stayed at #{dealer_total}.")
   end
+
   erb :'/game'
+end
+
+get '/game_over' do
+  erb :'game_over'
 end
 
 
