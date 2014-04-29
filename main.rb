39d7e24d366a3d3b23ae61f087d16f93105714b3
Josh Leeman <joshleeman@gmail.com>
2014-04-28 23:02:37 -0400

    downcase

--- main.rb
+++ main.rb
@@ -49,7 +49,7 @@ helpers do
   def display_card(card)
     suit = card[0]
     value = card[1]
-    "<img src='/images/cards/#{suit.downcase}_#{value}.jpg' style='margin-right:10px' />"
+    "<img src='/images/cards/#{suit.downcase}_#{value.downcase}.jpg' style='margin-right:10px' />"
   end
 
   def win!(message)
